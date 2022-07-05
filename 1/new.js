581ce84a440d6b1d83539d3bc326d18056e0b08b
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-05 18:36:16 +0900

    fix: #1 button design issue & task info frame

--- 1/new.js
+++ 1/new.js
@@ -3,11 +3,12 @@ const addButton = document.querySelector('button');
 
 const taskList = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
 
-addButton.addEventListener('click', (e) => {
+addButton.addEventListener('click', () => {
     const taskTitle = newInput[0].value;
     const taskCategory = newInput[1].value;
 
     taskList.push({
+        'id': taskList.length,
         'title': taskTitle,
         'category': taskCategory,
         'isDone': false
