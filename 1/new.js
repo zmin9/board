d3aa50275dc28e53d43ac6c10d2413db480d4e92
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-05 14:57:30 +0900

    add: #1 add new.js for adding task feature

---
+++ 1/new.js
@@ -0,0 +1,19 @@
+const newInput = document.querySelectorAll('.task-input > input');
+const addButton = document.querySelector('button');
+
+const taskList = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+
+addButton.addEventListener('click', (e) => {
+    const taskTitle = newInput[0].value;
+    const taskCategory = newInput[1].value;
+
+    taskList.push({
+        'title': taskTitle,
+        'category': taskCategory,
+        'isDone': false
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+    newInput[0].value = '';
+    newInput[1].value = '';
+});
