5cd2dcaaceef57c441df7679a6687ee597fced4e
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-06 17:08:23 +0900

    add: #1 add feature deleting task
    5. 기타
    - contextmenu 이벤트로 테스크 삭제 가능

--- 1/main.js
+++ 1/main.js
@@ -71,4 +71,40 @@ taskList.forEach((task, idx) => {
         e.preventDefault();
         locateTaskBox(taskBox);
     });
+
+    taskBox.addEventListener('contextmenu', (e) => {
+        e.preventDefault();
+
+        const taskTitle = document.createElement('div');
+        taskTitle.className = 'modal-title';
+        taskTitle.innerText = task.title;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.className = 'large-button';
+        deleteBtn.innerText = '삭제';
+
+        const modal = document.createElement('div');
+        modal.classList.add('modal', 'modal-position-center', 'flex-alignment-center');
+        modal.append(taskTitle, deleteBtn);
+
+        modal.children[1].addEventListener('click',()=>{
+            taskList.splice(idx, 1);
+
+            localStorage.setItem('tasks', JSON.stringify(taskList));
+            document.body.lastChild.remove();
+            taskBox.parentNode.removeChild(taskBox);
+        });
+
+        const background = document.createElement('div');
+        background.addEventListener('click', (e) => {
+            e.preventDefault();
+            document.body.lastChild.remove();
+        });
+        background.className = 'modal-background';
+
+        const modalWithBackground = document.createElement('div');
+        modalWithBackground.append(background, modal);
+        this.blur();
+        document.body.appendChild(modalWithBackground);
+    });
 });
