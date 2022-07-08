2e4280aecb6c6271905828e1ff0ad6c282b6e495
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-08 18:05:11 +0900

    feat: #1 add color transition & input focus style

--- 1/js/categoryButton.js
+++ 1/js/categoryButton.js
@@ -2,7 +2,7 @@ function createCategoryButton(category) {
     const categoryButton = document.createElement('div');
     categoryButton.innerText = category;
     categoryButton.id = category;
-    categoryButton.classList.add('blue-round-box', 'padding-8', 'font-size-14');
+    categoryButton.classList.add('blue-round-box', 'padding-8', 'font-size-14', 'color-transition');
     if (category === currentCategory) categoryButton.classList.add('selected-category');
 
     return categoryButton;
