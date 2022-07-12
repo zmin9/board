713c70d92e2594590317e883b422c2c3117b2793
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-12 18:08:33 +0900

    chore: #2 init assignment

---
+++ 2/js/addPage/main.js
@@ -0,0 +1,83 @@
+const [taskTitle, taskCategory] = document.querySelectorAll('.task-input');
+
+let selectedCategory = '+';
+
+function categoryBtnWithEvent(category){
+    const categoryBtn = createCategoryButton(category);
+    categoryBtn.addEventListener('click', () => {
+        if (selectedCategory !== category) {
+            categoryBtn.classList.add('bg-blue-color');
+            for (let i = 0; i < categories.children.length; i++) {
+                if (categories.children[i].id === selectedCategory) {
+                    categories.children[i].classList.remove('bg-blue-color');
+                    break;
+                }
+            }
+            selectedCategory = category;
+            taskCategory.value = selectedCategory;
+            taskCategory.disabled = true;
+        }
+    });
+    return categoryBtn
+}
+
+categoryList.forEach((category)=>{
+    if (category !== '' && category !== '전체') {
+        const categoryBtn = categoryBtnWithEvent(category);
+        categories.appendChild(categoryBtn);
+    }
+});
+
+{
+    const plusCategoryBtn = createCategoryButton('+');
+    plusCategoryBtn.classList.add('bg-blue-color');
+
+    plusCategoryBtn.addEventListener('click', () => {
+        plusCategoryBtn.classList.add('bg-main-color');
+        for (let i = 0; i < categories.children.length - 1; i++) {
+            categories.children[i].classList.remove('bg-main-color');
+        }
+        selectedCategory = '+';
+        taskCategory.value = '';
+        taskCategory.disabled = false;
+        taskCategory.focus();
+    });
+    categories.appendChild(plusCategoryBtn);
+}
+
+const addButton = document.querySelector('button');
+
+addButton.addEventListener('click', () => {
+    const modal = document.querySelector('.modal');
+    const cautionMsg = document.querySelector('.caution');
+
+    if(taskTitle.value.trim() === ''){
+        cautionMsg.style.visibility = 'visible';
+        taskTitle.value = '';
+        taskTitle.focus();
+        return;
+    }
+
+    taskList.push({
+        'id': taskList.length,
+        'title': taskTitle.value.trim(),
+        'category': taskCategory.value.trim(),
+        'isDone': false
+    });
+
+    if(selectedCategory==='+' && !categoryList.has(taskCategory.value.trim())){
+        categories.lastChild.before(categoryBtnWithEvent(taskCategory.value.trim()));
+    }
+    selectedCategory = '+';
+
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+
+    taskTitle.value = '';
+    taskCategory.value = '';
+    cautionMsg.style.visibility = 'hidden';
+
+    modal.style.opacity = '100';
+    setTimeout(() => {
+        modal.style.opacity = '0';
+    },3000);
+});
---
+++ 2/js/mainPage/manageTaskBox.js
@@ -0,0 +1,56 @@
+function createTaskBox(task, i) {
+    const taskTitle = document.createElement('span');
+    taskTitle.classList.add('task-title', 'font-weight-500', 'line-height-24', 'font-color-main');
+    taskTitle.append(task.title);
+
+    const taskCategory = document.createElement('div');
+    taskCategory.classList.add('task-category', 'margin-top-8', 'font-weight-600', 'font-color-sub');
+    taskCategory.append(task.category);
+
+    const taskInformation = document.createElement('div');
+    taskInformation.classList.add('task-info');
+    taskInformation.append(taskTitle, taskCategory);
+
+    const taskCheckbox = document.createElement('div');
+    taskCheckbox.className = 'task-check';
+
+    const labelForCheckbox = document.createElement('label');
+    labelForCheckbox.classList.add('flex-box-row');
+    labelForCheckbox.htmlFor = 'task' + String(i);
+    labelForCheckbox.append(taskCheckbox, taskInformation);
+
+    const checkboxTypeInput = document.createElement('input');
+    checkboxTypeInput.type = 'checkbox';
+    checkboxTypeInput.id = 'task' + String(i);
+    checkboxTypeInput.checked = task.isDone;
+
+    const taskBox = document.createElement('div');
+    taskBox.classList.add('task-box', 'margin-top-16', 'flex-box-row');
+    taskBox.listNum = i;
+    taskBox.append(checkboxTypeInput, labelForCheckbox);
+
+    return taskBox;
+}
+
+function locateTaskBox(taskBox) {
+    const taskIdx = taskBox.listNum;
+    taskList[taskIdx].isDone = taskBox.querySelector('input[type="checkbox"]').checked;
+
+    const targetList = taskList[taskIdx].isDone ? tasksDone : tasksInProgress;
+    if (targetList.children.length === 0) {
+        targetList.append(taskBox);
+    } else {
+        let isAppended = false;
+        for (let i = 0; i < targetList.children.length; i++) {
+            if (taskBox.listNum < targetList.children[i].listNum) {
+                targetList.children[i].before(taskBox);
+                isAppended = true;
+                break;
+            }
+        }
+        if (!isAppended) targetList.appendChild(taskBox);
+    }
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+
+    checkProgress();
+}
