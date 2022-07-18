5e28e273bfb8f23c362ec0320d2a86983daf30e3
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-18 16:21:49 +0900

    refactor: #2 change function declaration(setCategoryBtnForMain)

--- 2/src/mainPage/main.js
+++ 2/src/mainPage/main.js
@@ -6,21 +6,21 @@ import showTaskBoxElem from "./manageTaskBox.js";
 data.setCurCategory('전체');
 
 setTodayDate();
-setCategoryBtn(['전체', ...data.categoryArr()], categoryContainer);
-updateTaskView(data.filteredTaskArr());
+setCategoryBtnForMain(['전체', ...data.categoryArr()]);
+setTaskView(data.filteredTaskArr());
 
-function setTodayDate (){
+function setTodayDate () {
     const day = new Date();
     document.querySelector('#today').append(`${day.getFullYear()}년 ${day.getMonth() + 1}월 ${day.getDate()}일`);
 }
 
-function setCategoryBtn(categoryArr, container) {
+function setCategoryBtnForMain(categoryArr) {
     categoryArr.forEach((category) => {
         const categoryBtn = createCategoryBtn(category);
         categoryBtn.addEventListener('click', () => {
             setTaskView(data.filteredTaskArr());
         });
-        container.appendChild(categoryBtn);
+        categoryContainer.appendChild(categoryBtn);
     });
 }
 
