44c80582a46c8e346c9d113f623fa30942133041
o3o0903@gmail.com <o3o0903@gmail.com>
2022-08-11 17:36:34 +0900

    feat: add filter transition.ts

---
+++ board/src/styles/transition.ts
@@ -0,0 +1,11 @@
+export const filterTransition = `
+  transition: filter;
+  transition-duration: 0.2s;
+  
+  :hover {
+    filter: brightness(0.9);
+  }
+  :active {
+    filter: brightness(0.8);
+  }
+`;
